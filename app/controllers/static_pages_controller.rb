083a3c76a0df3003c30825e58f6b0aa887f1dd9d
Varun Bhartia <vbhartia@mba2014.hbs.edu>
2014-03-16 00:10:48 -0400

    updated tabs

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,26 +1,13 @@
 class StaticPagesController < ApplicationController
 
   def homepage
-  	#if user_signed_in?
-  	#	redirect_to dashboard_path
-  	#end
-  	@at_home_page = true
+	redirect_to payroll_history_path
+
 
   end
 
   def layout_test
   end
 
-  def resource_name
-    :user
-  end
-
-  def resource
-    @resource ||= User.new
-  end
-
-  def devise_mapping
-    @devise_mapping ||= Devise.mappings[:user]
-  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Calendar::Application.routes.draw do
   #********** Static Pages ************#
   root :to => "static_pages#homepage"
 
-  get '/payroll_history' => "static_pages#layout_test"
+  get '/payroll_history' => "static_pages#layout_test", as: 'payroll_history'
 
 end
